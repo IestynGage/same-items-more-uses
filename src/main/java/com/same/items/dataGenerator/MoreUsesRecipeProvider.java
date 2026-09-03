f86d8c8dbd9d47a6fe01d1764872dc4b3ffe97f5
Iestyn Gage <33298011+IestynGage@users.noreply.github.com>
2026-09-03 20:11:59 +0100

    Add poison potato to posion potion

--- src/main/java/com/same/items/dataGenerator/MoreUsesRecipeProvider.java
+++ src/main/java/com/same/items/dataGenerator/MoreUsesRecipeProvider.java
@@ -2,18 +2,17 @@ package com.same.items.dataGenerator;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.fabricmc.fabric.api.registry.FabricPotionBrewingBuilder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.crafting.CookingBookCategory;
 import net.minecraft.world.item.crafting.Ingredient;
 import java.util.concurrent.CompletableFuture;
-
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.Item;
 
 public class MoreUsesRecipeProvider extends FabricRecipeProvider {
 
@@ -26,16 +25,25 @@ public class MoreUsesRecipeProvider extends FabricRecipeProvider {
     return new RecipeProvider(registryLookup, exporter) {
       @Override
       public void buildRecipes() {
+//        TODO: Check other cooking things work as well
         SimpleCookingRecipeBuilder.smelting(
-            Ingredient.of(Items.POWDER_SNOW_BUCKET),
-            RecipeCategory.MISC,
-            CookingBookCategory.MISC,
-            Items.WATER_BUCKET,
-            0.7f,
-            50
+                Ingredient.of(Items.POWDER_SNOW_BUCKET),
+                RecipeCategory.MISC,
+                CookingBookCategory.MISC,
+                Items.WATER_BUCKET,
+                0.7f,
+                50
             )
             .unlockedBy("has_powder_snow_bucket", has(Items.POWDER_SNOW_BUCKET))
             .save(this.output, "something.smelt_powder_snow_bucket");
+
+        FabricPotionBrewingBuilder.BUILD.register(builder -> {
+          builder.addMix(
+              Potions.AWKWARD,
+              Items.POISONOUS_POTATO,
+              Potions.POISON
+          );
+        });
       }
     };
   }
