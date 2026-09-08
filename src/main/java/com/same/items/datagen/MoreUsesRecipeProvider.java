86b844d2bb3b5f7de905019af3f298e48a37e899
Iestyn Gage <33298011+IestynGage@users.noreply.github.com>
2026-09-08 22:35:07 +0100

    Add powder snow bucket related recipes

--- src/main/java/com/same/items/datagen/MoreUsesRecipeProvider.java
+++ src/main/java/com/same/items/datagen/MoreUsesRecipeProvider.java
@@ -2,11 +2,9 @@ package com.same.items.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
+import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.CookingBookCategory;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -35,6 +33,22 @@ public class MoreUsesRecipeProvider extends FabricRecipeProvider {
             .unlockedBy("has_powder_snow_bucket", has(Items.POWDER_SNOW_BUCKET))
             .save(this.output, "something.smelt_powder_snow_bucket");
 
+        shapeless(RecipeCategory.MISC, Items.POWDER_SNOW_BUCKET)
+            .requires(Items.SNOW_BLOCK)
+            .requires(Items.BUCKET)
+            .unlockedBy("has_bucket", has(Items.BUCKET))
+            .save(this.output, "something.smelt_powder_snow_bucket");
+
+        shapeless(RecipeCategory.MISC, Items.POWDER_SNOW_BUCKET)
+            .requires(Items.SNOWBALL, 8)
+            .requires(Items.BUCKET)
+            .unlockedBy("has_bucket", has(Items.BUCKET))
+            .save(this.output, "something.smelt_powder_snow_bucket");
+
+        shapeless(RecipeCategory.MISC, Items.SNOWBALL, 8)
+            .requires(Items.POWDER_SNOW_BUCKET)
+            .unlockedBy("has_bucket", has(Items.POWDER_SNOW_BUCKET))
+            .save(this.output, "something.smelt_powder_snow_bucket");
       }
     };
   }
